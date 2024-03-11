c2082e2c067073ceaec09ebe13f99635cc8bc683
Joshua Calafell <joshcalafell@gmail.com>
2024-03-11 04:22:23 -0700

    scaffolding:products-line

--- libs/product-detail/src/lib/product-detail/product-detail.component.spec.ts
+++ libs/product-detail/src/lib/product-detail/product-detail.component.spec.ts
@@ -3,8 +3,8 @@ import { ButtonRowComponent } from '@rfs-atomic/button-row'
 import { DetailGroupComponent } from '@rfs-atomic/detail-group'
 import { HashesComponent } from '@rfs-atomic/hashes'
 import { ProductRatingsComponent } from '@rfs-atomic/product-ratings'
-import { ProductDetailComponent } from './product-detail.component'
 import { of } from 'rxjs'
+import { ProductDetailComponent } from './product-detail.component'
 import { candleMock } from './product.mock'
 
 describe('ProductDetailComponent', () => {
@@ -36,31 +36,5 @@ describe('ProductDetailComponent', () => {
 	it('should render product name', () => {
 		const compiled = fixture.nativeElement
 		expect(compiled.querySelector('.product-name')).toBeTruthy()
-	})
-
-	it('should render product description', () => {
-		const compiled = fixture.nativeElement
-		expect(compiled.querySelector('.product-description')).toBeTruthy()
-	})
-
-	it('should render product details info', () => {
-		const compiled = fixture.nativeElement
-		expect(compiled.querySelector('#details-header').innerHTML).toContain(
-			'Details'
-		)
-	})
-
-	it('should render product attributes info', () => {
-		const compiled = fixture.nativeElement
-		expect(compiled.querySelector('#attributes-header').innerHTML).toContain(
-			'Attributes'
-		)
-	})
-
-	it('should render product subscription info group 3', () => {
-		const compiled = fixture.nativeElement
-		expect(compiled.querySelector('#subscription-header').innerHTML).toContain(
-			'Subscription'
-		)
 	})
 })
