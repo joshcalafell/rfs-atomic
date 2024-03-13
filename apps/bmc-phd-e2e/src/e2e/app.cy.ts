6428b9f487e8f2594f3ae13c1c0bfe2291190162
Joshua Calafell <joshcalafell@gmail.com>
2024-03-12 19:31:32 -0700

    chore:specs

--- apps/bmc-phd-e2e/src/e2e/app.cy.ts
+++ apps/bmc-phd-e2e/src/e2e/app.cy.ts
@@ -2,12 +2,4 @@ import { getGreeting } from '../support/app.po'
 
 describe('bmc-phd-e2e', () => {
 	beforeEach(() => cy.visit('/'))
-
-	it('should display welcome message', () => {
-		// Custom command example, see `../support/commands.ts` file
-		cy.login('my-email@something.com', 'myPassword')
-
-		// Function helper example, see `../support/app.po.ts` file
-		getGreeting().contains(/Bernadette Calafell Ph.D./)
-	})
 })
